6c95cf08447b3ab68ecfdc0eb439b625a1001b70
Bruninha <Brunanotube@gmail.com>
2023-06-20 20:55:03 -0300

    .

--- JavaScripit/jmain.js
+++ JavaScripit/jmain.js
@@ -1,37 +1,67 @@
-const casas=document.querySelectorAll('casa');
-const disco=document.querySelector('disco');
+import { getTabuleiro } from "./states.js";
 
-console.log (casa);
-console.log(disco);
-
-disco.addEventListener('dragstar', comecaArrastar);
-for (let i = 0; i<casas.length; i++){
-const casa= casas[i];
-casa.addEventListener('drop', passouPorCima);
-casa.addEventListener('drop', recebeAlgo);
-}
-
-let arrastado =null;
+const eTabuleiro = document.querySelector('.Tabuleiro');
 
 
-function comecaArrastar(evento){
-   
+let arrastado = null;
+
+setup();
+
+
+function comecaArrastar(evento) {
+
     arrastado = evento.target;
 
 }
 
-function recebeAlgo (evento){
-    
-    if(arrastado){
+function recebeAlgo(evento) {
+
+    if (arrastado) {
         evento.target.appendChild(arrastado);
         arrastado = null
     }
 
 }
 
-function passouPorCima (evento){
-    
-    evento.preventDefault();
-    
+function passouPorCima(evento) {
 
+    evento.preventDefault();
+
+
+}
+
+function setup() {
+    const tabuleiro = getTabuleiro();
+    for (let i = 0; i < tabuleiro.length; i++) {
+        const casa = tabuleiro[i];
+        const eCasa = criaCasa(casa);
+        eTabuleiro.appendChild(eCasa);
+       
+    }
+}
+
+function criaCasa(casa) {
+    const eCasa = document.createElement('div');
+    eCasa.classList.add("casa");
+    eCasa.addEventListener('dragover', passouPorCima);
+    eCasa.addEventListener('drop', recebeAlgo);
+    if (casa){
+        const eDisco= criaDisco(casa);
+        eCasa.appendChild (eDisco);
+    }
+
+    return eCasa;
+}
+
+function criaDisco (casa) {
+    const eDisco = document.createElement ('div');
+    eDisco.classList.add('disco');
+    eDisco.addEventListener('dragstart', comecaArrastar);
+    if (casa=='p') {
+        eDisco.classList.add('preto');
+     } else {
+            eDisco.classList.add('branco')
+
+    }
+return eDisco;
 }
