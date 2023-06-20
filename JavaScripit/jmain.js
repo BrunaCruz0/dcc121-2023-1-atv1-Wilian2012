7745ed5217482402fb8770a2f88295582b66386f
Bruninha <Brunanotube@gmail.com>
2023-06-20 20:04:25 -0300

    Adicionar elementos de arrastar e soltar

--- JavaScripit/jmain.js
+++ JavaScripit/jmain.js
@@ -1,5 +1,37 @@
-const casa=document.querySelector('casa');
+const casas=document.querySelectorAll('casa');
 const disco=document.querySelector('disco');
 
 console.log (casa);
 console.log(disco);
+
+disco.addEventListener('dragstar', comecaArrastar);
+for (let i = 0; i<casas.length; i++){
+const casa= casas[i];
+casa.addEventListener('drop', passouPorCima);
+casa.addEventListener('drop', recebeAlgo);
+}
+
+let arrastado =null;
+
+
+function comecaArrastar(evento){
+   
+    arrastado = evento.target;
+
+}
+
+function recebeAlgo (evento){
+    
+    if(arrastado){
+        evento.target.appendChild(arrastado);
+        arrastado = null
+    }
+
+}
+
+function passouPorCima (evento){
+    
+    evento.preventDefault();
+    
+
+}
